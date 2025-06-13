c22bbeba7dcad6354153d9009a87db9404f8c85b
Swechha13 <swechaworkholics@gmail.com>
2025-06-13 17:41:52 +0530

    first commit

---
+++ Variables.js
@@ -0,0 +1,10 @@
+let a=5
+const author="swechha"
+//author=5 //Throse an error because constant can't be changed
+let b="swechha";
+// let b=4; let can't be redecla
+{
+    let b='this'
+    console.log(b);
+}
+console.log(b);
---
+++ datatypes.js
@@ -0,0 +1,19 @@
+//js is case sensitive
+// primitive data types nn ss bb u
+let a=null
+let b=345
+let c= true;//can also be false
+let d=BigInt("876688") + BigInt("3")
+let e= "swechha"
+let f= Symbol("hello")
+let g=undefined
+console.log(a, b, c, d, e, f)
+console.log(typeof d)
+
+// objects in js
+const item={
+    "swechha":true,
+    "ansh": false,
+    "lovish":undefined
+}
+console.log(item["swechha"])
